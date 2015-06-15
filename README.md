7daf3de9a744e52544a57d10da2f0cc0b9a0f2bd
Rohan Roy <b4you0870@gmail.com>
2015-06-15 19:50:23 +0530

    Confrentu App using Google AppEngine

--- README.md
+++
@@ -1,2 +0,0 @@
-# confrentu
-A Conference App
---
+++ settings.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = "430434980997-p24qmabf2kqk3mel7kq25u4n97bl3ct7.apps.googleusercontent.com"
+
